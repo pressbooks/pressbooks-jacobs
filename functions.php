1d85eff009215a721fb02ba772a985f090545887
Ned Zimmerman <ned@bight.ca>
2017-12-01 13:52:55 -0400

    Cleanup.

--- functions.php
+++ functions.php
@@ -7,7 +7,3 @@
 add_action( 'after_setup_theme', function () {
 	// TODO
 } );
-
-add_filter( 'pb_global_components_path', function( $path ) {
-	return dirname( __FILE__ ) . '/node_modules/buckram/styles/';
-} );
